1782b4d9e508139dbbff65b071e2c2da340cbb2a
Fiaz <Fiaz@192.168.1.4>
2017-03-20 18:23:13 +0500

    file is committed

---
+++ src/classes/practice/product.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.practice;
+
+/**
+ *
+ * @author Fiaz
+ */
+public class product {
+    public static void main(String[] args){
+           int a,b,c;
+           a=10;
+           b=30;
+          c= a*b;
+          System.out.println("the product is"+c);
+    }
+    
+}
