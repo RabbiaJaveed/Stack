45db4781837923c585019866cfeb4e091b557248
Fiaz <Fiaz@192.168.1.4>
2017-03-20 18:29:39 +0500

    file is committed

---
+++ src/classes/practice/differce.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.practice;
+
+/**
+ *
+ * @author Fiaz
+ */
+public class differce {
+    
+   public static void main(String args[])
+   {
+      int x, y, z;
+      x=60;
+      y=30;
+      z = x - y;
+      System.out.println("diference  of entered integers = "+z);
+   }
+            
+            
+    
+}
